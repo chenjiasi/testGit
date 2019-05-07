c760109a4f67516436d861206a63ed4fe4e41e0b
chenjiasi <14795618241@163.com>
2019-05-07 13:33:08 +0800

    b add box

---
+++ src/components/Box.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+.box{
+    width:100px;
+    height:100px;
+    background:red;
+}
+</style>
+
